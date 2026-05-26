266cba2878bcdcbb3418369c63635bcf71dbe3d2
szcleiton <souzacleiton54@gmail.com>
2026-05-25 23:08:03 -0300

    chore: Implementação do S3 Storage Adapter

--- qrcode.generator/src/main/java/qrcode/generator/infrastructure/S3StorageAdapter.java
+++ qrcode.generator/src/main/java/qrcode/generator/infrastructure/S3StorageAdapter.java
@@ -1,30 +1,40 @@
 package qrcode.generator.infrastructure;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import qrcode.generator.ports.StoragePort;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.ByteArrayInputStream;
+@Component
+public class S3StorageAdapter implements StoragePort {
 
-//public class S3StorageAdapter implements StoragePort {
-//
-//    private final S3Client s3client;
-//    private final String bucketName;
-//    private final String region;
-//
-//    public S3StorageAdapter(@Value("${aws.S3.region}") String region,
-//                            @Value("${aws.s3.bucket-name}") String bucketName) {
-//        this.bucketName = bucketName;
-//        this.region = region;
-//        this.s3client = S3Client.builder()
-//                .region(Region.of(this.region))
-//                .build();
-//    }
-//
-//    @Override
-//    public string uploadFile(byte[] fileData, String fileName, String contentType) {
-//        PutObjectRequest
-//    }
-//}
+    private final S3Client s3client;
+    private final String bucketName;
+    private final String region;
+
+    public S3StorageAdapter(@Value("${aws.S3.region}") String region,
+                            @Value("${aws.s3.bucket-name}") String bucketName) {
+        this.bucketName = bucketName;
+        this.region = region;
+        this.s3client = S3Client.builder()
+                .region(Region.of(this.region))
+                .build();
+    }
+
+    @Override
+    public String uploadFile(byte[] fileData, String fileName, String contentType) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(contentType)
+                .build();
+
+        s3client.putObject(putObjectRequest, RequestBody.fromBytes(fileData));
+
+        return String.format("https://%$.s3.amazonaws.com/%$",
+                bucketName, region, fileName);
+    }
+}
